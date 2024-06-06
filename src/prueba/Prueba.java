dc1bc60b254ff58900fa68744c4192e9c691b074
alisson <alisson.cfuentes0312@gmail.com>
2024-06-06 13:20:50 -0500

    carga inicial

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,19 @@
+
+package prueba;
+import java.*;
+
+public class Prueba {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int num1 = 6, num2 = 7, suma;
+        suma = num1 + num2;
+        System.out.print("La suma es: "+suma);
+        
+        suma = suma + 5;
+        System.out.print(suma);
+    }
+    
+}
